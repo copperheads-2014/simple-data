33f785bb02bd526fda03266ea8e60211113006c9
Eleni Chappen & Grant Hummer <pair+echappen.gphummer@devbootcamp.com>
2014-12-19 20:54:59 -0600

    removed validation for organization_id from user model so user does not need to be a part of an organization before being created. this will allow the user to create or join an organization after user account creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :organization_id, presence: true
 
   belongs_to :organization
 end
