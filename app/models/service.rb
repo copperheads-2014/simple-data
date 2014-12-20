871a2cf211c3f60e079e8171612b35eb3d2ad0c0
Eleni Chappen & Grant Hummer <pair+echappen.gphummer@devbootcamp.com>
2014-12-20 14:15:32 -0600

    Added make slug method to create a URI slug before a Service object is created. Also added a validation to ensure that names are unique.

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,18 @@ class Service
   field :organization_id, type: Integer
   field :description, type: String
   field :name, type: String
+  field :slug, type: String
   embeds_many :records
+
+  before_create :make_slug
+
+  validates :name, uniqueness: {case_sensitive: false}
+
+  protected
+
+  def make_slug
+    self.slug = self.name.split(" ").map(&:downcase).join("-")
+  end
+
 end
 
