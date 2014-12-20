f49bc6157fad98f3ae2530cf38b4d2270b38af8f
Eleni Chappen & Grant Hummer <pair+echappen.gphummer@devbootcamp.com>
2014-12-20 14:24:57 -0600

    added routes to route file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   #   resources :records, only: [:index, :show]
   # end
 
-  get "/services/:service_name", to: "service#show"
+  get "/services/:service_slug", to: "services#show"
+  get "/services", to: "services#index"
+  get "/services/new", to: "services#new"
+  post "/services", to: "services#create"
+  get "/services/:service_slug/edit", to: "services#edit"
+  put "/services/:service_slug", to: "services#update"
+  delete "/services/:service_slug", to: "services#destroy"
+
+  get "/services/:service_slug/records", to: "records#index"
 
 
 end
