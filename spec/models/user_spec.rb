3d13e993a58abfb823cf33a5bd00fad07fd69e1d
Eleni Chappen & Grant Hummer <pair+echappen.gphummer@devbootcamp.com>
2014-12-19 19:58:05 -0600

    added two tests for user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#initialize" do
+    it "adds a user to the database" do
+      test_user = User.create(name: "The Greek", email: "Plato@athens.gr", organization_id: 1)
+      expect(User.find(test_user.id).name).to eq("The Greek")
+    end
+
+    it "fails to add a user when required fields are missing" do
+      roman = User.create(name: "The Roman", email: "Cato@Rome.it")
+      # roman.should have(1).error_on(:organization_id)
+      # expect(User.find(1).name).to return_error
+    end
+
+  end
+
+
 end
