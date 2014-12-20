ab02124dda5c509e2fd26a84d0665c0d1807bcbc
Eleni Chappen & Grant Hummer <pair+echappen.gphummer@devbootcamp.com>
2014-12-19 19:57:50 -0600

    added validations for name, email, organization_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :organization_id, presence: true
+
   belongs_to :organization
 end
