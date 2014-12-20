4d88549065199dc3433c127c6e2cb0b105b87acd
Eleni Chappen & Grant Hummer <pair+echappen.gphummer@devbootcamp.com>
2014-12-19 20:53:19 -0600

    added custom validation to Organization model to ensure that an organization has at least one user

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,12 @@
 class Organization < ActiveRecord::Base
   has_many :users
+  validate :organization_has_users
+
+  def organization_has_users
+    if self.users.size == 0
+      errors.add("Organization must have at least one user")
+    end
+  end
 
   def services
     Service.where(organization_id: self.id)
