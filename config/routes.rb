37ed44fd63095d6856ae999f9a2071de1809785b
Eleni Chappen & Grant Hummer <pair+echappen.gphummer@devbootcamp.com>
2014-12-20 14:15:40 -0600

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :services do
-    resources :records, only: [:index, :show]
-  end
+  # resources :services do
+  #   resources :records, only: [:index, :show]
+  # end
+
+  get "/services/:service_name", to: "service#show"
 
 
 end
