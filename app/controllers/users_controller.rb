c423b4378fca8b108965c138668996ab13bef9d6
Ryan Jones <run0391@gmail.com>
2014-12-20 18:19:36 -0700

    added private method to set user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,10 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:users).permit(:name, :email, :password, :password_confirmation)
+  end
 end
