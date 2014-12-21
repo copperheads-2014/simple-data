e6f3adc4fa439b9938edc2f4a3535ec7ba667346
Ryan Jones <run0391@gmail.com>
2014-12-20 19:35:12 -0700

    edited routes for sessions to be structured through resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :users, except: [:index]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # resources :services do
   #   resources :records, only: [:index, :show]
