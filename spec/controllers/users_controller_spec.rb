2fde6ae10060996484f05841326f294ec8287d0c
Ryan Jones <run0391@gmail.com>
2014-12-20 21:17:05 -0700

    removed tests for nonexistent get routes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
 
   describe "GET new" do
     it "returns http success" do
