39c9d124e6446676ffb76f2ff5b378cdf6fad02d
Ryan Jones <run0391@gmail.com>
2014-12-20 19:02:35 -0700

    added validations to email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password, length: { minimum: 8}, on: :create
 
   belongs_to :organization
 
