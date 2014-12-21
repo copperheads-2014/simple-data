5e47f501706b912b3a9cde36fe03e5bf5f077add
Ryan Jones <run0391@gmail.com>
2014-12-20 21:17:24 -0700

    changed the auto-generated specs for routes to pending

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,28 +11,28 @@ RSpec.describe UsersController, :type => :controller do
   end
 
   describe "GET edit" do
-    it "returns http success" do
+    pending "returns http success" do
       get :edit
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET update" do
-    it "returns http success" do
+    pending "returns http success" do
       get :update
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET destroy" do
-    it "returns http success" do
+    pending "returns http success" do
       get :destroy
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET show" do
-    it "returns http success" do
+    pending "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
     end
