04c0858e3cd99a0ebea0da2c331ea5902fd5dbc5
Ryan Jones <run0391@gmail.com>
2014-12-20 18:51:28 -0700

    loads user show page upon successful user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |t|
+    respond_to do |format|
       if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
       else
       end
     end
