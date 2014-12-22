3d2e073b42fd26aafd42670e5e556633e779aca9
Ryan Jones <run0391@gmail.com>
2014-12-22 12:52:15 -0700

    removed edit and destroy until deemed necessary

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, except: [:index]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :organizations, only: [:new, :create, :edit, :destroy]
+  resources :organizations, only: [:new, :create]
 
   # resources :services do
   #   resources :records, only: [:index, :show]
