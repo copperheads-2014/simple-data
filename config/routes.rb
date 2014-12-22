860d9764e7a6534af82b68e87fd1e37a50cce355
Ryan Jones <run0391@gmail.com>
2014-12-22 16:44:44 -0700

    reordered routes so new can exist

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   #   resources :records, only: [:index, :show]
   # end
 root 'application#index'
+  get "/services/new", to: "services#new"
   get "/services/:service_slug", to: "services#show"
   get "/services", to: "services#index"
-  get "/services/new", to: "services#new"
   post "/services", to: "services#create"
   get "/services/:service_slug/edit", to: "services#edit"
   put "/services/:service_slug", to: "services#update"
