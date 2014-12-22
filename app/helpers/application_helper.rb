e129173fe8e8c33fdecb1a576e5254795e798666
Dave Chmura <dschmura@humbledaisy.com>
2014-12-22 08:55:30 -0500

    Added a page_title helper to the application helper. This sets a default base_title and combines it with a page title local variable (to be set in the controller actions). The default base_title (currently 'Simple Data') will always display and will be combined with the page_title if it is set. So, for example, on the landing page we could set the @page_title to be 'Home' and the resulting title would display as 'Home | Simple Data'. The order is set to accomodate usability and screen readers most effectively.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+ def page_title
+    base_title = "SimpleData"
+    @base_title = base_title
+    if @page_title.nil?
+      base_title
+    else
+      "#{@page_title}  | #{@base_title}"
+    end
+  end
+
 end
