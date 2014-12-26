10481a404db119014debfaf30c08c1bcde49ba66
Dave Chmura <dschmura@humbledaisy.com>
2014-12-26 11:28:50 -0500

    Updated page title application helper to fallback to using the controller/action if a page_title is not specified.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def page_title
     base_title = "Simple Data"
     if @page_title.nil?
-      base_title
+      "#{params[:controller].capitalize} - #{params[:action].capitalize} | " + base_title
     else
-      "#{@page_title}  | #{base_title}"
+      "#{@page_title} | #{base_title}"
     end
   end
 end
