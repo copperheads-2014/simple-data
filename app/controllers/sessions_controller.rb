7360f6a982859c320b33b1b9c65649dfab0b9a70
Ryan Jones <run0391@gmail.com>
2014-12-27 18:31:17 -0700

    after authentication redirected to page to upload a file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to new_user_path
+      redirect_to services_new_path
     end
   end
 
