1a39e62d23f9ca848ea1c94d3537eae06051dc9b
Ryan Jones <run0391@gmail.com>
2014-12-27 19:10:29 -0700

    wrote test for name presence validation

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe Service, :type => :model do
   let(:zoo) { Service.create(name: "Map of Zoos") }
 
+  describe 'validations' do
+    it 'requires the presence of a name' do
+      service = Service.create(organization_id: 1, description: "description", slug: "a", total_records: 1, version: 1)
+      expect(service.errors[:name]).to include("can't be blank")
+    end
+  end
+
   describe "#make_slug" do
     it "saves a slug to the database on creation" do
       expect(zoo.slug).to eq("map-of-zoos")
