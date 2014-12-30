779a9861a6665f58eafbeda8a742d1701a9f4b13
gphummer <gphummer@gmail.com>
2014-12-30 09:14:02 -0600

    git weirdness

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,6 @@ root 'application#index'
   put "/services/:service_slug", to: "services#update"
   delete "/services/:service_slug", to: "services#destroy"
 
-  match "/services/:service_slug", to: "services#metadata", via: [:options]
-
   get "/services/:service_slug/records", to: "records#index"
 
 
