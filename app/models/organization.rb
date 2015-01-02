3ad7b41312ccb1af7e9a23e463d7adafdc57b0c4
Ryan Jones <run0391@gmail.com>
2015-01-02 11:44:30 -0600

    organization adjusted for active record service model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,15 +1,6 @@
 class Organization < ActiveRecord::Base
   has_many :users
+  has_many :services
+
   validates :name, presence: true
-  # validate :organization_has_users
-
-  # def organization_has_users
-  #   if self.users.size == 0
-  #     errors.add("Organization must have at least one user")
-  #   end
-  # end
-
-  def services
-    Service.where(organization_id: self.id)
-  end
 end
