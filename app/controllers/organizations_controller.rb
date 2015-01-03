217cc72bfd3bf5eaa8aefd6587cfc74da261f7c4
Ryan Jones <run0391@gmail.com>
2015-01-03 15:53:34 -0600

    added description text area to new org form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -29,6 +29,6 @@ class OrganizationsController < ApplicationController
   private
 
   def organization_params
-    params.require(:organization).permit(:name)
+    params.require(:organization).permit(:name, :description)
   end
 end
