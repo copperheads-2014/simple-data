869ae828946425f898f05f11c052241e89e2a6ee
Grant Hummer <gphummer@gmail.com>
2015-01-03 13:19:49 -0600

    Added more integration tests

--- spec/features/simpledata_spec.rb
+++ spec/features/simpledata_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "User visits the website" do
+feature "Browsing the website" do
   scenario "User visits the landing page" do
     visit '/'
     expect(page).to have_text("Give us your data")
@@ -18,7 +18,54 @@ feature "User visits the website" do
     expect(page.current_path).to eq("/about_us")
   end
 
+  scenario "User can visit the 'Contact Us' page" do
+    visit '/'
+    click_link "Contact Us"
+    expect(page.current_path).to eq("/contact_us")
+  end
 
+  scenario "User can visit the FAQs page" do
+    visit '/'
+    click_link "FAQs"
+    expect(page.current_path).to eq("/faqs")
+  end
 
 end
+
+feature "Signing in" do
+  background do
+    User.create(name: "TheGreek", email: "Plato@athens.gr", organization_id: 1, password: 'password', password_confirmation: 'password')
+  end
+
+  scenario "Signing in with the correct credentials" do
+    visit "/sessions/new"
+
+    page.fill_in "session_email", with: "Plato@athens.gr"
+    page.fill_in "session_password", with: "password"
+    click_button "Login"
+
+    expect(page.current_path).to eq("/services/new")
+  end
+
+  scenario "Signing in with incorrect credentials" do
+    visit "/sessions/new"
+
+    page.fill_in "session_email", with: "Trojanscum@gmail.com"
+    page.fill_in "session_password", with: "Whatchulookinat"
+    click_button "Login"
+
+    expect(page).to have_text("Create a New User")
+  end
+end
+
+feature "Creating an account" do
+end
+
+feature "Uploading an API" do
+end
+
+feature "Retrieving data from API endpoints" do
+end
+
+
 
