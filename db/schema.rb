92b2ee2e1c75bca7c61c8447a4a0d5b4d16ec1df
Ryan Jones <run0391@gmail.com>
2015-01-03 17:47:43 -0600

    added migration to create Service Update table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150103220752) do
+ActiveRecord::Schema.define(version: 20150103223827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150103220752) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
+  end
+
+  create_table "service_updates", force: true do |t|
+    t.integer  "service_id"
+    t.integer  "user_id"
+    t.integer  "records_added"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "services", force: true do |t|
