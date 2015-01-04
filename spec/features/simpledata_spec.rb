46e8ca65c8e4c9d05459cbc526e6e828c3641f63
Grant Hummer <gphummer@gmail.com>
2015-01-03 21:27:35 -0600

    writing browse API tests, logged in user test isn't working yet

--- spec/features/simpledata_spec.rb
+++ spec/features/simpledata_spec.rb
@@ -36,7 +36,11 @@ feature "Browsing the website" do
     expect(page.current_path).to eq("/services")
   end
 
+<<<<<<< HEAD
   scenario "Logged in user can browse APIs" do
+=======
+  pending "Logged in user can browse APIs" do
+    #test not working yet
     greek = User.new(name: "TheGreek", email: "Plato@athens.gr", password: 'password', password_confirmation: 'password')
     delian = Organization.create(name: "Delian League", description: "A buncha Greeks")
     zoos = Service.create(name: "Map of Zoos", description: "Description")
@@ -51,6 +55,7 @@ feature "Browsing the website" do
 
     click_link "simple-data"
     click_link "Explore APIs"
+
     expect(page).to have_text("Your Organization's APIs:")
   end
 end
