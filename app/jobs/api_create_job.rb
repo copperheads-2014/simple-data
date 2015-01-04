2a978d65eba302cae10c8b06123d0364f8b1e781
Grant Hummer <gphummer@gmail.com>
2015-01-04 16:36:03 -0600

    added app/jobs, ApiCreateJob is working, ApiUpdateJob still in progress

---
+++ app/jobs/api_create_job.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+class ApiCreateJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(service_id, current_user_id, params)
+    #Get the file from S3
+    uploaded_csv = new_retrieve_file(params[:file])
+    #Insert the records as Mongo Documents as part of a Mongo Collection
+    service = Service.find(service_id)
+    service.create_records(uploaded_csv)
+    service.set_total_records
+    service.update(creator_id: current_user_id)
+    # Find or create tags and add them to the service
+    service.add_tags(params[:tags])
+  end
+
+  def new_retrieve_file(params)
+    file = open(params).read
+    CSV.new(file,
+      headers: true,
+      :converters => :all,
+      :header_converters => lambda { |h| h.downcase.gsub(' ', '_') unless h.nil? }
+      )
+  end
+
+end
