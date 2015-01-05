8504f633643c20fc3b50ecd3631c119c61e48d6c
Grant Hummer <gphummer@gmail.com>
2015-01-05 14:36:35 -0600

    added records methods to version model to reflect that version now has many records

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,6 +1,7 @@
 class Version < ActiveRecord::Base
 
   belongs_to :service
+  has_many :headers
   validates :number, presence: true
   validates :total_records, presence: true
 
@@ -10,9 +11,30 @@ class Version < ActiveRecord::Base
   #   self.total_records = 0
   # end
 
+  def records
+    Record.with(collection: self.collection)
+  end
+
+  def insert_record(record)
+    records.create!(record)
+  end
+
+  def collection
+    service.slug.underscore.camelize + "V#{number}"
+  end
+
   def set_total_records
     # TODO: replace with counter_column maybe?
     self.update(total_records: self.records.count)
+  end
+
+  def create_records(file)
+    last_count = total_records
+    file.each do |row|
+      row_hash = row.to_hash
+      row_hash[:insertion_id] = last_count += 1
+      insert_record(row_hash)
+    end
   end
 
 
