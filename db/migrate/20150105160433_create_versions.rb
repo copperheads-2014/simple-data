cca6abddb67593efdcfcb5caee37a136eb4f0ca4
Grant Hummer <gphummer@gmail.com>
2015-01-05 10:06:09 -0600

    added migration for version

---
+++ db/migrate/20150105160433_create_versions.rb
@@ -0,0 +1,11 @@
+class CreateVersions < ActiveRecord::Migration
+  def change
+    create_table :versions do |t|
+      t.integer :number
+      t.integer :service_id
+      t.boolean :active
+
+      t.timestamps null: false
+    end
+  end
+end
