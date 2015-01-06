7acbdbc520e6eace35d7d59ce8332bdd3f9471c5
Grant Hummer <gphummer@gmail.com>
2015-01-06 14:32:51 -0600

    fixed header matching and update_params

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -27,6 +27,7 @@ class ServicesController < ApplicationController
   end
 
   def create
+    update_params = {}
     @service = ServiceCreation.create(service_params, current_user)
     @service.organization_id = current_user.organization.id
     @service.versions.last.updates << VersionUpdate.create(filename: params[:service][:file])
@@ -67,19 +68,23 @@ class ServicesController < ApplicationController
 
   def update
     @service = Service.find_by(slug: params[:service_slug])
-    update_params = params[:update_params]
+    p params
+    update_params = params[:service][:update_params]
     respond_to do |format|
       #Ensure the individual submitting owns the organization
       if @service.save && (@service.organization_id == current_user.organization_id)
+        p "Before read"
         #Read in the posted file from S3
         update_csv = retrieve_file(params[:service][:file]).read
+        p "After read"
         if headers_match?(update_csv, @service)
+          p "Headers matching"
           # old_record_count = @service.records.count
           CsvImportJob.perform_later(@service.latest_version.updates.last.id,                         update_params,
                                      params[:service])
           format.html { redirect_to "/services/#{@service.slug}", notice: "Service was successfully updated."}
         else
-          format.html { redirect_to "/services/#{@service.slug}/edit"}
+          format.html { redirect_to "/services/#{@service.slug}/edit", notice: "The headers of your CSV file must match the example headers below. Either select that you want to create a new version, or edit your file so its headers match the headers of the current version."}
         end
       end
     end
@@ -103,7 +108,6 @@ class ServicesController < ApplicationController
     file = open(params).read
     CSV.new(file,
       headers: true,
-      :converters => :all,
       :header_converters => lambda { |h| h.downcase.gsub(' ', '_') unless h.nil? }
       )
   end
@@ -115,6 +119,8 @@ class ServicesController < ApplicationController
   def headers_match?(new_file, service)
     existing_headers = service.latest_version.headers.map { |header| header.name.downcase }
     new_file_headers = new_file.headers.map {|header| header.downcase }
+    p existing_headers
+    p new_file_headers
     new_file_headers.sort == existing_headers.sort
   end
 
