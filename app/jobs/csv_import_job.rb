15e71dc407662d7554d073d6c2817cf11a6e0741
Grant Hummer <gphummer@gmail.com>
2015-01-06 09:43:42 -0600

    refactored to account for updating files

--- app/jobs/csv_import_job.rb
+++ app/jobs/csv_import_job.rb
@@ -4,22 +4,36 @@ require 'open-uri'
 class CsvImportJob < ActiveJob::Base
   queue_as :default
 
-  def perform(version_update_id, params={})
+  def perform(version_update_id, updating=false, params={})
     @version_update = VersionUpdate.find(version_update_id)
     @version_update.update(status: :processing)
-    # download the file locally
+
+    if updating
+      append_with_same_headers
+      # replace_with_same_headers
+      # create_new_version
+    else
+      # download the file locally
+      file = download_file(@version_update.filename)
+      # parse the headers
+      headers = grab_headers(file)
+      p headers
+      # create the headers schema
+      create_headers_schema(headers)
+      # import the data
+      add_records(file, @version_update.version.id)
+      # delete the file
+      @version_update.update(status: :completed)
+      # rescue => e
+      #   @version_update.update(status: :failed)
+    end
+
+  end
+
+  def append_with_same_headers
     file = download_file(@version_update.filename)
-    # parse the headers
-    headers = grab_headers(file)
-    p headers
-    # create the headers schema
-    create_headers_schema(headers)
-    # import the data
-    create_records(file, @version_update.version.id)
-    # delete the file
+    add_records(file, @version_update.version.id)
     @version_update.update(status: :completed)
-  rescue => e
-    @version_update.update(status: :failed)
   end
 
   def download_file(filename)
@@ -40,18 +54,7 @@ class CsvImportJob < ActiveJob::Base
     end
   end
 
-  def create_records(file, version_id)
-    version = Version.find(version_id)
-    last_count = version.total_records
-    CSV.new(file, headers: true, header_converters: lambda { |h| h.downcase.gsub(' ', '_') unless h.nil? }
-      ).each do |row|
-      row_hash = row.to_hash
-      row_hash[:insertion_id] = last_count += 1
-      version.insert_record(row_hash)
-    end
-  end
-
-  def create_records(file, version_id)
+  def add_records(file, version_id)
     version = Version.find(version_id)
     last_count = version.total_records
     CSV.new(file, headers: true, header_converters: lambda { |h| h.downcase.gsub(' ', '_') unless h.nil? }
