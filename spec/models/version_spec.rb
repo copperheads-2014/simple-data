e219a49b2edbafe9539b98a5ba88c86da22519c3
Ryan Jones <run0391@gmail.com>
2015-01-05 22:39:14 -0600

    transferred test from service to version

--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Version, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:zoo) { Service.create(name: "Map of Zoos", description: "Description needs to be 12 characters.", organization_id: 1) }
+  let(:zoo_v1) {Version.create(number: 1, service_id: zoo.id, active: true, total_records: 0)}
+
+  describe "#set_total_records" do
+    it "sets total records to count of existing records" do
+      10.times { zoo_v1.records.create() }
+      zoo_v1.set_total_records
+      expect(zoo_v1.total_records).to eq(zoo_v1.records.count)
+    end
+  end
 end
