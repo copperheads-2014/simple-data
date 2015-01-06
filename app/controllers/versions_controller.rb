1265e8b7be3fa3bae1714eca6ba7077a61393707
Jordy Williams <jordythepoet@gmail.com>
2015-01-06 12:40:46 -0600

    versions index and show page is up

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,14 +1,14 @@
 class VersionsController < ApplicationController
   def index
     @service = Service.find_by(slug: params[:service_slug])
+    @versions = @service.versions
   end
 
   def show
     @service = Service.find_by(slug: params[:service_slug])
-    @version = @service.versions.find_by(id: params[:version_id])
+    @version = Version.find_by(service_id: @service.id)
     @organization = Organization.find_by(id: @service.organization_id)
-    @headers = @service.versions.find_by(id: 1).number
-    # @headers << "sample"
-    # @headers = @services.versions.find_by(id: params[:version_id]).headers.map(&:name)
+    # @headers = Header.find_by(version_id: params[:version_id]).map(&:name)
+    # Figure out headers
   end
 end
