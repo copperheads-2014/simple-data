7e399366f697eabb94cbf8387e24def55421842a
Grant Hummer <gphummer@gmail.com>
2015-01-06 11:15:42 -0600

    refactored and added functionality to create new versions for a service.

--- app/jobs/csv_import_job.rb
+++ app/jobs/csv_import_job.rb
@@ -4,35 +4,57 @@ require 'open-uri'
 class CsvImportJob < ActiveJob::Base
   queue_as :default
 
-  def perform(version_update_id, updating=false, params={})
+  def perform(version_update_id, updating=false, append=false, new_version=false, params={})
     @version_update = VersionUpdate.find(version_update_id)
     @version_update.update(status: :processing)
 
     if updating
-      append_with_same_headers
-      # replace_with_same_headers
-      # create_new_version
+      append_with_same_headers if append
+      # maybe write a method: replace_with_same_headers
+      create_new_version if new_version
     else
-      # download the file locally
-      file = download_file(@version_update.filename)
-      # parse the headers
-      headers = grab_headers(file)
-      p headers
-      # create the headers schema
-      create_headers_schema(headers)
-      # import the data
-      add_records(file, @version_update.version.id)
-      # delete the file
-      @version_update.update(status: :completed)
-      # rescue => e
-      #   @version_update.update(status: :failed)
+      create_first_version
     end
 
   end
 
+  def create_first_version
+    download_and_add_records
+    # parse the headers
+    headers = grab_headers(file)
+    # create the headers schema
+    create_headers_schema(headers, @version_update)
+    # rescue => e
+    #   @version_update.update(status: :failed)
+  end
+
   def append_with_same_headers
+    download_and_add_records
+  end
+
+  def create_new_version
     file = download_file(@version_update.filename)
+    service_to_update = @version_update.version.service
+    current_version = @version_update.version
+
+    new_version = Version.new(number: current_version.number + 1)
+    service_to_update.versions << new_version
+    new_version.make_version_update(@version_update.filename)
+
+    headers = grab_headers(file)
+    create_headers_schema(headers, new_version.updates.last)
+
+    add_records(file, new_version.id)
+
+    new_version.set_total_records
+  end
+
+  def download_and_add_records
+    # download the file locally
+    file = download_file(@version_update.filename)
+    # import the data
     add_records(file, @version_update.version.id)
+    # delete the file
     @version_update.update(status: :completed)
   end
 
@@ -48,9 +70,9 @@ class CsvImportJob < ActiveJob::Base
     return headers
   end
 
-  def create_headers_schema(headers)
+  def create_headers_schema(headers, version_update)
     headers.each do |header_name|
-      @version_update.version.headers << Header.create(name: header_name)
+      version_update.version.headers << Header.create(name: header_name)
     end
   end
 
