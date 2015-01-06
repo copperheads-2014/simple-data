6d375016f8f68aef4fcd3f0e01336f7a7e75ef20
Grant Hummer <gphummer@gmail.com>
2015-01-06 14:33:24 -0600

    fixed header assignment

--- app/jobs/csv_import_job.rb
+++ app/jobs/csv_import_job.rb
@@ -8,32 +8,34 @@ class CsvImportJob < ActiveJob::Base
     @version_update = VersionUpdate.find(version_update_id)
     @version_update.update(status: :processing)
 
+    p update_params
     if update_params[:updating]
+      p "HERE WE ARE UPDATING!!!!!!!!!!!!!!!!!!!!"
       append_with_same_headers if update_params[:append]
       # maybe write a method: replace_with_same_headers
       create_new_version if update_params[:new_version]
     else
+      p "HERE WE ARE CREATING!!!!!!!!!!!!!!!!!!!!"
       create_first_version
     end
 
   end
 
   def create_first_version
+    p "HERE WE ARE CREATING THE FIRST VERSION WITH HEADERS!!!!!!!!!!!!!!!!!!!!"
     @version_update.update(status: :processing)
-    download_and_add_records
-    # parse the headers
-    headers = grab_headers(file)
-    # create the headers schema
-    create_headers_schema(headers, @version_update)
+    download_and_add_records_and_set_headers
     # rescue => e
     #   @version_update.update(status: :failed)
   end
 
   def append_with_same_headers
+    p "HERE WE ARE CREATING AN API WITH NO HEADERS!!!!!!!!!!!!!!!!!!!!"
     download_and_add_records
   end
 
   def create_new_version
+    p "HERE WE ARE CREATING A NEW VERSION WITH HEADERS!!!!!!!!!!!!!!!!!!!!!!!"
     file = download_file(@version_update.filename)
     service_to_update = @version_update.version.service
     current_version = @version_update.version
@@ -50,11 +52,15 @@ class CsvImportJob < ActiveJob::Base
     new_version.set_total_records
   end
 
-  def download_and_add_records
+  def download_and_add_records_and_set_headers
     # download the file locally
     file = download_file(@version_update.filename)
     # import the data
     add_records(file, @version_update.version.id)
+    # parse the headers
+    headers = grab_headers(file)
+    # create the headers schema
+    create_headers_schema(headers, @version_update)
     # delete the file
     @version_update.update(status: :completed)
   end
@@ -72,6 +78,7 @@ class CsvImportJob < ActiveJob::Base
   end
 
   def create_headers_schema(headers, version_update)
+    p headers
     headers.each do |header_name|
       version_update.version.headers << Header.create(name: header_name)
     end
