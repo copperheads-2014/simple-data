c4d8b1fa491bea40743804ab0eaec7b5fd975121
Grant Hummer <gphummer@gmail.com>
2015-01-06 19:30:47 -0600

    slightly modified

--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Version, :type => :model do
   let(:zoo) { Service.create(name: "Map of Zoos", description: "Description needs to be 12 characters.", organization_id: 1) }
-  let(:zoo_v1) {Version.create(number: 1, service_id: zoo.id, active: true)}
+  let(:zoo_v1) {Version.create(service_id: zoo.id, active: true)}
 
   describe "#set_total_records" do
     it "sets total records to count of existing records" do
