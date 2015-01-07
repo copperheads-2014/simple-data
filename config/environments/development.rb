61611de5e9146c3c4555eb4c79f6b9fe2f13ea1d
Eleni Chappen <elenichappen@gmail.com>
2015-01-07 13:48:53 -0600

    more heroku config stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ENV["REDISTOGO_URL"] = "redis://redistogo:f2f99490ecccd8094dda0ea49e57d637@mummichog.redistogo.com:9926/"
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
