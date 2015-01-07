07d8a5d795017bde5ce122b55b9d1bffca8594ec
Grant Hummer <gphummer@gmail.com>
2015-01-06 19:02:55 -0600

    Initial total records set to 0

--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Version, :type => :model do
-  let!(:zoo) { Service.create(name: "Map of Zoos", description: "Description needs to be 12 characters.", organization_id: 1) }
-  let(:zoo_v1) {Version.create(number: 1, service_id: zoo.id, active: true, total_records: 0)}
+  let(:zoo) { Service.create(name: "Map of Zoos", description: "Description needs to be 12 characters.", organization_id: 1) }
+  let(:zoo_v1) {Version.create(number: 1, service_id: zoo.id, active: true)}
 
   describe "#set_total_records" do
     it "sets total records to count of existing records" do
@@ -11,4 +11,13 @@ RSpec.describe Version, :type => :model do
       expect(zoo_v1.total_records).to eq(zoo_v1.records.count)
     end
   end
+
+  describe "#set_initial_total_records" do
+    it "sets total records to 0 when a version is created" do
+      union = Service.create(name: "The Union", description: "How old are these civil war analogies getting?", organization_id: 1)
+      union.make_version
+      expect(union.latest_version.total_records).to eq(0)
+    end
+  end
+
 end
