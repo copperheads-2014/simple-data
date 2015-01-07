73ad04c569230bee7a35419c09bbbeba771e6afb
Jordy Williams <jordythepoet@gmail.com>
2015-01-07 09:20:53 -0600

    took off desc character limits

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,7 +8,7 @@ class Service < ActiveRecord::Base
 
   validates :organization_id, presence: true
   validates :name, presence: true, uniqueness: {case_sensitive: false}
-  validates :description, presence: true, length: { :in => 12..300 }
+  validates :description, presence: true
   validates :slug, presence: true, on: :save
 
   after_create :make_version
