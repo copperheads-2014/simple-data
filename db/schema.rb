a0d90d1c4906c0f3572bfcd9f0d147e8a61b4688
Ryan Jones <run0391@gmail.com>
2015-01-24 16:03:50 -0700

    schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107223332) do
+ActiveRecord::Schema.define(version: 20150124225009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20150107223332) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "token"
   end
 
   create_table "version_updates", force: :cascade do |t|
