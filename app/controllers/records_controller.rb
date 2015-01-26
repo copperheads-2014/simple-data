8a35b15027eae42244c321dd5f9ca70d3d9f3f6f
Ryan Jones <run0391@gmail.com>
2015-01-26 11:35:35 -0700

    put logic into controller method to render proper status if no token

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,17 +2,21 @@ class RecordsController < ApplicationController
   attr_reader :settings
 
   def index
-    @service = Service.find_by(slug: params[:service_slug])
-    v_num = params[:version][1..-1]
-    @version = @service.versions.find_by(number: v_num)
-    if @service.activated && @version.active
-      @settings = default_params.merge(params)
-      @records = RecordQueryService.new(@version, settings).fetch_records
-      @formatter = DataFormatter.new(metadata_options)
-      @formatter.data = @records.to_json(json_options)
-      render json: @formatter.to_json(except: ["created_at","updated_at","id"]), status: 200
+    if authorized
+      @service = Service.find_by(slug: params[:service_slug])
+      v_num = params[:version][1..-1]
+      @version = @service.versions.find_by(number: v_num)
+      if @service.activated && @version.active
+        @settings = default_params.merge(params)
+        @records = RecordQueryService.new(@version, settings).fetch_records
+        @formatter = DataFormatter.new(metadata_options)
+        @formatter.data = @records.to_json(json_options)
+        render json: @formatter.to_json(except: ["created_at","updated_at","id"]), status: 200
+      else
+        render json: "This service has been deactivated. Check the service's documentation for details."
+      end
     else
-      render json: "This service has been deactivated. Check the service's documentation for details."
+      render json: DataFormatter.new(), status: 401
     end
   end
 
